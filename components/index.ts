4196faa56efc1b35ea37b3965e7a2bbdfb1ac296
Hersh Thakur <hersh.thakur@gmail.com>
2023-10-22 17:29:51 +0100

    created the inital structure

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Button from "./Button";
+import Hero from "./Hero";
+import Guide from "./Guide";
+import Features from "./Features";
+import Camp from "./Camp";
+import GetApp from "./GetApp";
+
+
+export { GetApp, Camp, Navbar, Footer, Button, Hero, Guide, Features };
