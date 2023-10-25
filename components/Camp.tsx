25653bfe03898a83eb678d740f5a33c5304b698e
Hersh Thakur <hersh.thakur@gmail.com>
2023-10-25 10:53:01 +0100

    adds guide section to app

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,5 +1,98 @@
+import Image from "next/image";
+import { PEOPLE_URL } from "@constants";
+
+type CampSiteProps = {
+  backgroundImg: string;
+  title: string;
+  subtitle: string;
+  peopleJoined: string;
+};
+
+const CampSite = ({
+  backgroundImg,
+  title,
+  subtitle,
+  peopleJoined,
+}: CampSiteProps) => {
+  return (
+    <div
+      className={`h-full w-full min-w-[1100px] ${backgroundImg} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
+    >
+      <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-20">
+        <div className="flexCenter gap-4">
+          <div>
+            <Image src="/folded-map.svg" width={50} height={50} alt="map" />
+          </div>
+          <div className="flex flex-col gap-1">
+            <h4 className="bold-18 text-white">{title}</h4>
+            <p className="regular-14 text-white">{subtitle}</p>
+          </div>
+        </div>
+
+        <div className="flexCenter gap-6">
+          <span className="flex -space-x-4 overflow-hidden">
+            {PEOPLE_URL.map((url) => (
+              <Image
+                key={url}
+                src={url}
+                width={52}
+                height={52}
+                alt="people"
+                className="inline-block h-10 w-10 rounded-full"
+              />
+            ))}
+          </span>
+          <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Camp = () => {
-  return <div>Camp</div>;
+  return (
+    <section className="2xl:max-container relative flex flex-col  py-10 lg:py-20 xl:mb-20">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+        <CampSite
+          backgroundImg="bg-bg-img-1"
+          title="Putuk Truno Camp"
+          subtitle="Some where in Malang"
+          peopleJoined="50+ Joined"
+        />
+        <CampSite
+          backgroundImg="bg-bg-img-1"
+          title="Mountain View Camp"
+          subtitle="Prigen, Pasuruan"
+          peopleJoined="20+ Joined"
+        />
+        <CampSite
+          backgroundImg="bg-bg-img-1"
+          title="My House Camp"
+          subtitle="I am not saying"
+          peopleJoined="5+ Joined"
+        />
+      </div>
+      <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+        <div className="lg:mac-w-[500px] relative w-full overflow-hidden rounded-3xl bg-green-50 p-8 xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20">
+          <h2>
+            <strong>Feeling Lost</strong> And Not Knowing The Way?
+          </h2>
+          <p className="regular-14 xl:regular-16 mt-5 text-white">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos aut
+            illum itaque explicabo nihil, nisi aperiam quo, qui facilis minus
+            provident ratione ea modi eum deserunt debitis libero maxime unde!
+          </p>
+          <Image
+            src="/quote.svg"
+            alt="quote"
+            width={186}
+            height={219}
+            className="camp-quote"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Camp;
